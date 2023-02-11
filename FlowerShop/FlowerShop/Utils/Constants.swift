ad4a04b4343a494b3d69fdc4700a1c15a06b38a7
George Rosescu <george.rosescu@gmail.com>
2023-02-11 12:04:29 +0200

    assets setup

---
+++ FlowerShop/FlowerShop/Utils/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  FlowerShop
+//
+//  Created by George Rosescu on 11.02.2023.
+//
+
+import Foundation
+
+struct Constants {
+    struct Fonts {
+        static let regular = "Poppins-Regular"
+        static let semiBold = "Poppins-SemiBold"
+        static let bold = "Poppins-Bold"
+
+        static let caption: CGFloat = 16
+        static let body: CGFloat = 20
+        static let smallBody: CGFloat = 18
+        static let title: CGFloat = 28
+    }
+}
---
+++ FlowerShop/FlowerShop/Utils/Extensions/Color+Utils.swift
@@ -0,0 +1,28 @@
+//
+//  Color+Utils.swift
+//  FlowerShop
+//
+//  Created by George Rosescu on 11.02.2023.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static var wolfPink: Color {
+        Color("WolfPink")
+    }
+
+    static var wolfWhite: Color {
+        Color("WolfWhite")
+    }
+
+    static var wolfGreen: Color {
+        Color("WolfGreen")
+    }
+
+    static var wolfBlue: Color {
+        Color("WolfBlue")
+    }
+}
+
