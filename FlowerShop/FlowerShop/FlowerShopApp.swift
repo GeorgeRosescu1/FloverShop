f270ad088e48527538d63aedaa6ca6a16e71d661
George Rosescu <george.rosescu@gmail.com>
2023-02-11 11:01:31 +0200

    initial commit

---
+++ FlowerShop/FlowerShop/FlowerShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerShopApp.swift
+//  FlowerShop
+//
+//  Created by George Rosescu on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
