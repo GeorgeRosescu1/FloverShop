14660c4b8bc582648cd95d5e9c103c07154142bb
George Rosescu <george.rosescu@gmail.com>
2023-02-11 14:52:46 +0200

    our customers button setup

--- FlowerShop/FlowerShop/Screens/Home/HomeView.swift
+++ FlowerShop/FlowerShop/Screens/Home/HomeView.swift
@@ -19,17 +19,27 @@ struct HomeView: View {
                     HStack {
                         Text("Comenzi")
                             .font(.custom(Constants.Fonts.bold, size: Constants.Fonts.title))
-                            .padding(.top, Constants.defaultPadding)
                         Spacer()
+                        Button {
+                            print("users")
+                        } label: {
+                            Image(systemName: "person.2")
+                                .foregroundColor(.wolfPink)
+                                .font(.custom(Constants.Fonts.bold, size: Constants.Fonts.body))
+                        }
+                        .frame(width: 40, height: 40)
+                        .background { Color.white }
+                        .cornerRadius(5)
                     }
+                    .padding(.top, Constants.defaultPadding)
 
                     VStack {
                         ForEach(viewModel.state.orders) { order in
                             OrderCell(order: order)
-                                .padding(.bottom, Constants.defaultPadding)
+                                .padding(.bottom, Constants.smallPadding)
                         }
                     }
-                    .padding(.top, 40)
+                    .padding(.top, Constants.defaultPadding)
                 }
                 .padding(.horizontal, Constants.defaultPadding)
             }
